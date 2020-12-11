55609c5c6a310e6b6553afebbd5547fe6b4d3edb
snowy881 <69603602+snowy881@users.noreply.github.com>
2020-12-11 15:57:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+  <body>
+    <script>
+      var part1 = "We're still here ";
+      var part2 = "selling real fake doors";
+      document.write (part1 + part2);
+    <script/>
+  </body>
+</html>
